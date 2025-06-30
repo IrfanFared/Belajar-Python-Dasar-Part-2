39b590b8ead82a0e175b2cc9440d8d783660aba7
Irfan <irfanganteng@gmail.com>
2025-06-30 22:05:17 +0700

    Add standard_libary.py with math import and factorial calculation

---
+++ standard_libary.py
@@ -0,0 +1,7 @@
+import math
+
+nilai_pi = math.factorial(12)
+#kenapa kita mengunakan titik saat mengakses standard libry?
+# karena titi sebagai penghubunh module > clas > funtion 
+
+print(f"nilai pi {nilai_pi}")
