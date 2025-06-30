6ef484397016e7c6f08f131d6e81813b11d042a0
Irfan <irfanganteng@gmail.com>
2025-06-30 21:33:39 +0700

    Add module1 import to main.py and initialize mypackage with tambah function

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import mypackage.module1
+import mypackage.module1 # ini adalah carannya megunakan package yang kiata buat
 
 
 hasi_tambah = mypackage.module1.tambah(12,12)
---
+++ mypackage/__init__.py
@@ -0,0 +1,2 @@
+from .module1 import tambah # beginilaha cara mendeklarasikan module yang kita buat ke file ini
+#setalah saya coba tanpa mendeklarasika teryata bida kita gunakan hanya saja lebih baik kita deklarsikan
